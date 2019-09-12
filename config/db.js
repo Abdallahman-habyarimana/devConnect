f55f8c8e5e38da7ae7430f05d1cfa8becce90f24
habdallah005 <43259755+habdallah005@users.noreply.github.com>
2019-09-11 21:31:38 -0400

    Back end User Model

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+// Connect to the database
+// Whenever used async rapit with try and catch
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    // message when the database is successful connected
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// Defines routes
+
+app.use("/api/users", require("./Routes/api/users"));
+app.use("/api/auth", require("./Routes/api/auth"));
+app.use("/api/profile", require("./Routes/api/profile"));
+app.use("/api/post", require("./Routes/api/post"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
