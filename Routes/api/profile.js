4600ea61ff45b23f78d399409e700b664d0a8f8a
habdallah005 <43259755+habdallah005@users.noreply.github.com>
2019-09-14 20:54:06 -0400

    Create & Update

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -110,7 +110,7 @@ router.post(
       // Create
       profile = new Profile(profileFields);
 
-      await Profile.save();
+      await profile.save();
       res.json(profile);
     } catch (err) {
       console.error(err.message);
